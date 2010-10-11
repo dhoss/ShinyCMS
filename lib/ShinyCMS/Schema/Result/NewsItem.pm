e725490534327285d4eaf071c484b6026a68314a
Devin Austin <dhoss@cpan.org>
2010-10-11 10:24:04 -0600

    updated NewsItem with TimeStamp stuff

--- lib/ShinyCMS/Schema/Result/NewsItem.pm
+++ lib/ShinyCMS/Schema/Result/NewsItem.pm
@@ -11,7 +11,7 @@ use MooseX::NonMoose;
 use namespace::autoclean;
 extends 'DBIx::Class::Core';
 
-__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "EncodedColumn");
+__PACKAGE__->load_components( "InflateColumn::DateTime", "TimeStamp", "EncodedColumn" );
 
 =head1 NAME
 
@@ -61,22 +61,22 @@ __PACKAGE__->table("news_item");
 =cut
 
 __PACKAGE__->add_columns(
-  "id",
-  { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
-  "author",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
-  "title",
-  { data_type => "varchar", is_nullable => 0, size => 100 },
-  "url_title",
-  { data_type => "varchar", is_nullable => 0, size => 100 },
-  "body",
-  { data_type => "text", is_nullable => 0 },
-  "posted",
-  {
-    data_type     => "timestamp",
-    default_value => \"current_timestamp",
-    is_nullable   => 0,
-  },
+    "id",
+    { data_type => "integer", is_auto_increment => 1, is_nullable => 0 },
+    "author",
+    { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+    "title",
+    { data_type => "varchar", is_nullable => 0, size => 100 },
+    "url_title",
+    { data_type => "varchar", is_nullable => 0, size => 100 },
+    "body",
+    { data_type => "text", is_nullable => 0 },
+    "posted",
+    {
+        data_type     => "timestamp",
+        default_value => \"current_timestamp",
+        is_nullable   => 0,
+    },
 );
 __PACKAGE__->set_primary_key("id");
 
@@ -91,17 +91,23 @@ Related object: L<ShinyCMS::Schema::Result::User>
 =cut
 
 __PACKAGE__->belongs_to(
-  "author",
-  "ShinyCMS::Schema::Result::User",
-  { id => "author" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
+    "author",
+    "ShinyCMS::Schema::Result::User",
+    { id            => "author" },
+    { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
 );
-
 
 # Created by DBIx::Class::Schema::Loader v0.07001 @ 2010-08-04 00:50:25
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:DqaH6gmhMhPOG4kjJLS2EQ
 
-
+__PACKAGE__->add_column(
+    "posted",
+    {
+        data_type     => "datetime",
+        set_on_create => 1,
+        is_nullable   => 0,
+    },
+);
 
 # EOF
 __PACKAGE__->meta->make_immutable;
